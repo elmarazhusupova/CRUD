61c3d912106e2a35c013f4347a09e4275cec8934
kachigos <chyngyzsubhanov@gmail.com>
2022-02-08 18:17:02 +0600

    n

---
+++ crud/gridApp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from gridApp.forms import EmployerForm
+from gridApp.models import Employes
+
+# Create your views here.
+
+def addnew(request):
+    if request.method == "POST":
+        form = EmployerForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/')
+            except:
+                pass
+    else:
+        form = EmployerForm()
+    return render(request, 'gridApp/index.html', {'form': form})
+
+
+def index(request):
+    employees = Employes.objects.all()
+    return render(request, "gridApp/show.html", {'employees': employees})
+
+
+def delete_all(request):
+    employees = Employes.objects.all()
+    employees.delete()
+    return redirect('/')
+
+
+def edit(request, id):
+    employee = Employes.objects.get(id=id)
+    return render(request,'gridApp/edit.html', {'employee':employee})
+def update(request, id):
+    employee = Employes.objects.get(id=id)
+    form = EmployerForm(request.POST, instance = employee)
+    if form.is_valid():
+        form.save()
+        return redirect("/")
+    return render(request, 'gridApp/edit.html', {'employee': employee})
+def delete(request, id):
+    employee = Employes.objects.get(id=id)
+    employee.delete()
+    return redirect("/")
